066c844cc74e1abefb24a0d5adc374f761475f40
Artem Ervits <generic13@gmail.com>
2019-08-23 15:57:52 -0400

    fixed buckets in ddl

--- images/phoenix-pqs-flask-stack/schema.sql
+++ images/phoenix-pqs-flask-stack/schema.sql
@@ -1,4 +1,4 @@
 -- schema file for pqs flask app
 create schema if not exists hits;
-create table if not exists hits.hits (id integer primary key) sal_buckets=4;
+create table if not exists hits.hits (id integer primary key) salt_buckets=4;
 create sequence if not exists hits.hit_sequence start 1 increment by 1 cache 10;
